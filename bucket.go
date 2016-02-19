f5e74e63b32d7d515bed9c3e95e2464468b71496
Mark Holton <holtonma@gmail.com>
2016-02-18 16:44:20 -0800

    clean up golint messages on Exported methods within bucket.go
    * add comments per golang conventions so the Exported methods appear in Godoc

--- bucket.go
+++ bucket.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// An interface representing a single bucket within a cluster.
+// Bucket is an interface representing a single bucket within a cluster.
 type Bucket struct {
 	name     string
 	password string
@@ -51,35 +51,49 @@ func createBucket(config *gocbcore.AgentConfig) (*Bucket, error) {
 	return bucket, nil
 }
 
+// OperationTimeout returns the int64 Duration encoded timeout for accessing a Bucket
 func (b *Bucket) OperationTimeout() time.Duration {
 	return b.opTimeout
 }
+
+// SetOperationTimeout allows for setting a Bucket timeout with an int64 time.Duration
 func (b *Bucket) SetOperationTimeout(timeout time.Duration) {
 	b.opTimeout = timeout
 }
+
+// DurabilityTimeout returns the specified Bucket durability timeout in int64 time.Duration
 func (b *Bucket) DurabilityTimeout() time.Duration {
 	return b.duraTimeout
 }
+
+// SetDurabilityTimeout specifies the Bucket durability timeout via an int64 time.Duration
 func (b *Bucket) SetDurabilityTimeout(timeout time.Duration) {
 	b.duraTimeout = timeout
 }
+
+// DurabilityPollTimeout returns the specified Bucket durability poll timeout in int64 time.Duration
 func (b *Bucket) DurabilityPollTimeout() time.Duration {
 	return b.duraPollTimeout
 }
+
+// SetDurabilityPollTimeout sets the specified Bucket's durability poll timeout via an int64 time.Duration
 func (b *Bucket) SetDurabilityPollTimeout(timeout time.Duration) {
 	b.duraPollTimeout = timeout
 }
 
+// SetTranscoder sets the Bucket's transcoder
 func (b *Bucket) SetTranscoder(transcoder Transcoder) {
 	b.transcoder = transcoder
 }
 
+// InvalidateQueryCache Invalidates and clears the query cache. This method can be used to explicitly clear the internal N1QL query cache. This cache will be filled with non-adhoc query statements (query plans) to speed up those subsequent executions. Triggering this method will wipe out the complete cache, which will not cause an interruption but rather all queries need to be re-prepared internally. This method is likely to be deprecated in the future once the server side query engine distributes its state throughout the cluster.
 func (b *Bucket) InvalidateQueryCache() {
 	b.queryCacheLock.Lock()
 	b.queryCache = make(map[string]*n1qlCache)
 	b.queryCacheLock.Unlock()
 }
 
+// Cas is acronym for "Check and Set" and is useful for ensuring that a mutation of a document by one user or thread does not override another near simultaneous mutation by another user or thread. The CAS value is returned by the server with the result when you perform a read on a document using Get or when you perform a mutation on a document using Insert, Upsert, Replace or Remove.
 type Cas gocbcore.Cas
 type pendingOp gocbcore.PendingOp
 
@@ -108,20 +122,24 @@ func (b *Bucket) getN1qlEp() (string, error) {
 	return n1qlEps[rand.Intn(len(n1qlEps))], nil
 }
 
+// Close the instance’s underlying socket resources.  Note that operations pending on the connection may fail.
 func (b *Bucket) Close() {
 	b.client.Close()
 }
 
+// IoRouter returns a pointer to the Bucket's client
 func (b *Bucket) IoRouter() *gocbcore.Agent {
 	return b.client
 }
 
-// *INTERNAL*
 // Internal methods, not safe to be consumed by third parties.
+// *INTERNAL*
+// TODO: gocb maintainers should make this unexported, and it shouldn't refer to an unexported type in return
 func (b *Bucket) Internal() *bucketInternal {
 	return b.internal
 }
 
+// Manager returns a pointer to a BucketManager
 func (b *Bucket) Manager(username, password string) *BucketManager {
 	return &BucketManager{
 		bucket:   b,
