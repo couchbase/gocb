53ee8b8c63aa09e1c9b6519a0cf0c41762131eff
Mark Holton <holtonma@gmail.com>
2016-02-18 16:44:20 -0800

    clean up golint messages on Exported methods within bucket.go
    * add comments per golang conventions so the Exported methods appear in Godoc
    
    Change-Id: I26a33195d43728386ac36dbf202ebf887d191e76
    Reviewed-on: http://review.couchbase.org/63855
    Reviewed-by: Brett Lawson <brett19@gmail.com>
    Tested-by: Brett Lawson <brett19@gmail.com>

--- bucket.go
+++ bucket.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-// An interface representing a single bucket within a cluster.
+// Bucket is an interface representing a single bucket within a cluster.
 type Bucket struct {
 	cluster   *Cluster
 	name      string
@@ -52,21 +52,32 @@ func createBucket(cluster *Cluster, config *gocbcore.AgentConfig) (*Bucket, erro
 	return bucket, nil
 }
 
+// OperationTimeout returns the int64 Duration encoded timeout for accessing a Bucket
 func (b *Bucket) OperationTimeout() time.Duration {
 	return b.opTimeout
 }
+
+// SetOperationTimeout allows for setting a Bucket timeout with an int64 time.Duration
 func (b *Bucket) SetOperationTimeout(timeout time.Duration) {
 	b.opTimeout = timeout
 }
+
+// DurabilityTimeout returns the specified Bucket durability timeout in int64 time.Duration
 func (b *Bucket) DurabilityTimeout() time.Duration {
 	return b.duraTimeout
 }
+
+// SetDurabilityTimeout specifies the Bucket durability timeout via an int64 time.Duration
 func (b *Bucket) SetDurabilityTimeout(timeout time.Duration) {
 	b.duraTimeout = timeout
 }
+
+// DurabilityPollTimeout returns the specified Bucket durability poll timeout in int64 time.Duration
 func (b *Bucket) DurabilityPollTimeout() time.Duration {
 	return b.duraPollTimeout
 }
+
+// SetDurabilityPollTimeout sets the specified Bucket's durability poll timeout via an int64 time.Duration
 func (b *Bucket) SetDurabilityPollTimeout(timeout time.Duration) {
 	b.duraPollTimeout = timeout
 }
@@ -83,14 +94,17 @@ func (b *Bucket) SetN1qlTimeout(timeout time.Duration) {
 	b.n1qlTimeout = timeout
 }
 
+// SetTranscoder sets the Bucket's transcoder
 func (b *Bucket) SetTranscoder(transcoder Transcoder) {
 	b.transcoder = transcoder
 }
 
+// InvalidateQueryCache Invalidates and clears the query cache. This method can be used to explicitly clear the internal N1QL query cache. This cache will be filled with non-adhoc query statements (query plans) to speed up those subsequent executions. Triggering this method will wipe out the complete cache, which will not cause an interruption but rather all queries need to be re-prepared internally. This method is likely to be deprecated in the future once the server side query engine distributes its state throughout the cluster.
 func (b *Bucket) InvalidateQueryCache() {
 	b.cluster.InvalidateQueryCache()
 }
 
+// Cas is acronym for "Check and Set" and is useful for ensuring that a mutation of a document by one user or thread does not override another near simultaneous mutation by another user or thread. The CAS value is returned by the server with the result when you perform a read on a document using Get or when you perform a mutation on a document using Insert, Upsert, Replace or Remove.
 type Cas gocbcore.Cas
 type pendingOp gocbcore.PendingOp
 
@@ -127,11 +141,13 @@ func (b *Bucket) getFtsEp() (string, error) {
 	return ftsEps[rand.Intn(len(ftsEps))], nil
 }
 
+// Close the instance’s underlying socket resources.  Note that operations pending on the connection may fail.
 func (b *Bucket) Close() {
 	b.cluster.closeBucket(b)
 	b.client.Close()
 }
 
+// IoRouter returns a pointer to the Bucket's client
 func (b *Bucket) IoRouter() *gocbcore.Agent {
 	return b.client
 }
@@ -142,6 +158,7 @@ func (b *Bucket) Internal() *bucketInternal {
 	return b.internal
 }
 
+// Manager returns a pointer to a BucketManager
 func (b *Bucket) Manager(username, password string) *BucketManager {
 	userPass := userPassPair{username, password}
 	if username == "" || password == "" {
