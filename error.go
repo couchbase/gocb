129dd558f901af9be48e93922950edf18eb2dd82
Brett Lawson <brett19@gmail.com>
2017-01-20 12:23:23 -0800

    Do not discard sub-document marshaling errors.
    Change-Id: Iafe3dd01ee02ba99d3e8e346a46faccf18968853

--- error.go
+++ error.go
@@ -3,7 +3,39 @@ package gocb
 import (
 	"errors"
 	"gopkg.in/couchbase/gocbcore.v2"
+	"strings"
 )
+
+// MultiError encapsulates multiple errors that may be returned by one method.
+type MultiError struct {
+	Errors []error
+}
+
+func (e *MultiError) add(err error) {
+	if multiErr, ok := err.(*MultiError); ok {
+		e.Errors = append(e.Errors, multiErr.Errors...)
+	} else {
+		e.Errors = append(e.Errors, err)
+	}
+}
+
+func (e *MultiError) get() error {
+	if len(e.Errors) == 0 {
+		return nil
+	} else if len(e.Errors) == 1 {
+		return e.Errors[0]
+	} else {
+		return e
+	}
+}
+
+func (e *MultiError) Error() string {
+	var errors []string
+	for _, err := range e.Errors {
+		errors = append(errors, err.Error())
+	}
+	return strings.Join(errors, ", ")
+}
 
 type clientError struct {
 	message string
