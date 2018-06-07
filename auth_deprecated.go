d8a958dfa0e7aa2c194e8b4e72a8305df529cd7a
Dmytro Lahoza <dlahoza@gdfsi.com>
2018-06-07 11:06:22 +0300

    Left only conventional "like alias" implementation

--- auth_deprecated.go
+++ auth_deprecated.go
@@ -1,7 +1,12 @@
-//+build go1.9
-
 package gocb
 
 // CertificateAuthenticator is included for backwards compatibility only.
 // Deprecated: Use CertAuthenticator instead.
-type CertificateAuthenticator = CertAuthenticator
+type CertificateAuthenticator struct {
+	CertAuthenticator
+}
+
+// Credentials returns the credentials for a particular service.
+func (ca CertificateAuthenticator) Credentials(req AuthCredsRequest) ([]UserPassPair, error) {
+	return ca.CertAuthenticator.Credentials(req)
+}
