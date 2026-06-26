d7a7c0d4364cd656777f5e298b65f7847d08ab09
Dimitris Christodoulou <dimitris.christodoulou@couchbase.com>
2026-06-26 14:01:00 +0100

    GOCBC-1839: Add support for CounterEq bounds type
    * Counters are now scoped at the performer-level rather than per-HorizontalScaleRunner
    * Added the counterEqualityBoundsExecutor
    * Implemented the SetCounter and ClearAllCounters RPCs
    
    Change-Id: I7d564a775671a290897dd0d4862a23d6852c6c0a
    Reviewed-on: https://review.couchbase.org/c/gocb/+/247619
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Charles Dixon <chvckd@gmail.com>
    Reviewed-on: https://review.couchbase.org/c/gocb/+/251210

--- internal/cmd/fit-performer/bounds.go
+++ internal/cmd/fit-performer/bounds.go
@@ -24,6 +24,22 @@ func (executor *counterBoundsExecutor) CanExecute() bool {
 	return executor.counter.GetAndDecrement() >= 0
 }
 
+type counterEqualityBoundsExecutor struct {
+	initialValue int32
+	counter      *counter.Counter
+}
+
+func newCounterEqualityBoundsExecutor(counter *counter.Counter) *counterEqualityBoundsExecutor {
+	return &counterEqualityBoundsExecutor{
+		initialValue: counter.Get(),
+		counter:      counter,
+	}
+}
+
+func (executor *counterEqualityBoundsExecutor) CanExecute() bool {
+	return executor.counter.Get() == executor.initialValue
+}
+
 type timeBoundsExecutor struct {
 	deadline time.Time
 }
